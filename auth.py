e0afef1cf6fba6cad3adbbd04db76535884eeb23
fabalinks <Fabian.stocek@gmail.com>
2021-10-19 10:05:04 +0200

    made authentication and dictionary

--- auth.py
+++ auth.py
@@ -3,5 +3,22 @@ def get_credentital():
     password = input("type password")
     return username, password
 
+def authenticate(username,password,pwdb):
+    auth=False
+    if username in pwdb:
+        if password == pwdb[username]:
+            print('Authentication succesful')
+            auth = True
+        else:
+            print('wrong password ')
+    else:
+        print ('User not known! ')
+    return auth
+
+pwdb = {'fabian':'sickpass',
+        'sandra': 'greatpwd'}
 username,password = get_credentital()
-print (username, password)
+authenticate(username,password,pwdb)
+
+
+
